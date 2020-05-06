328c000cd8434ba3fd61e8dd6efdb9986f076c14
Wayne Luong <wayneluong93@gmail.com>
2020-05-06 03:06:48 +0100

    Update About.js

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -9,7 +9,7 @@ export default class About extends Component {
         <p>User Manager is a ReactJS-based web app for creating, reading, updating and deleting(CRUD) user data.</p>
         <p>It uses JSON placeholder data to send and receive HTTP requests to a server like an actual database.</p>
         <p>NOTE: New users cannot be edited as JSON placeholder only mimics a HTTP request</p>
-        <p>For more info about the creator, please visit: <a href='https://www.wayneluong.github.io'>https://wayneluong.github.io/</a></p>
+        <p>For more info about the creator, please visit: <a href='https://wayneluong.github.io/'>https://wayneluong.github.io/</a></p>
       </div>
     )
   }
